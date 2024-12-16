0aa856d7add86c64e3b09fb96fd72b6f358843b1
DSH <deeksharaole@gmail.com>
2024-12-16 19:33:54 +0530

    Add files via upload
    This is the initial commit to accomplish stage 1 interview task i.e front end UI

---
+++ Script1.js
@@ -0,0 +1,37 @@
+﻿function toggleBox(id) {
+    // Get all boxes
+    const boxes = document.querySelectorAll('.box');
+
+    // Loop through all boxes and collapse them
+    boxes.forEach(box => {
+        if (box.id !== id) {
+            box.classList.remove('expanded');
+        }
+    });
+
+
+    // Toggle the selected box
+    const selectedBox = document.getElementById(id);
+    selectedBox.classList.toggle('expanded');
+   // selectedBox.style.borderBlockColor = "limegreen";
+
+
+    // Check the radio button when box is expanded
+    const radioButton = selectedBox.querySelector('input[type="radio"]');
+    radioButton.checked = !radioButton.checked;
+}
+
+/*.custom - radio {
+    display: flex;
+    align - items: center;
+    font - size: 18px;
+    color: darkslategrey;
+    cursor: pointer;
+}
+*/
+
+
+
+
+
+
